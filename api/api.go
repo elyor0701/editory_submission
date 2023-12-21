aa6ac7c59f0b71b65fed964da3e9da4839c46731
euralov <287-euralov@users.noreply.gitlab.udevs.io>
2023-12-21 19:09:44 +0500

    bug fixed

--- api/api.go
+++ api/api.go
@@ -145,7 +145,7 @@ func customCORSMiddleware() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Role")
 		c.Header("Access-Control-Max-Age", "3600")
 
 		if c.Request.Method == "OPTIONS" {
--- migrations/postgres/000004_create_table.up.sql
+++ migrations/postgres/000004_create_table.up.sql
@@ -1,4 +1,4 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE "keyword" (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
